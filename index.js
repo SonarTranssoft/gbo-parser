6849da92c9b55b6ef56724028009f3d96b362464
SonarTransssoft <support@sonar55.ru>
2020-09-17 17:39:58 +0600

    Commit behind changes

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const Chapter = require('./models/chapter');
+let URL = 'https://www.mirgaza.ru/catalog/'
 
-function getDataFromCatalog() {
-    let URL = 'https://www.mirgaza.ru/catalog/';
-    return axios({
+function getChaptersFromCatalog() {
+     return axios({
         method: "GET",
         url: URL
     });
 }
 
-getDataFromCatalog()
+getChaptersFromCatalog()
     .then(res => {
         const $ = cheerio.load(res.data);
         let listOfCatalog = $('.catalog').children().toArray();
         let arrayOfIds = listOfCatalog.map(el => el.attribs.id);
-        let chapters = arrayOfIds.map(el => $('#' + el).text().trim());
-        console.log(chapters);
+        let chapters = arrayOfIds.map(el => {
+            return new Chapter($('#' + el).text().trim(), $('#' + el).find('a').attr('href').substring(9, this.length))
+        })
+        console.log(chapters)
     })
     .catch(err => console.error(err));
+
 
---
+++ models/chapter.js
@@ -0,0 +1,8 @@
+class Chapter {
+    constructor(title, urlAddress) {
+        this.title = title;
+        this.urlAddress = urlAddress;
+    }
+}
+
+module.exports = Chapter;
