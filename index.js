383dc546a1d6acbfae07d056b4dd84cf20b26514
SonarTransssoft <support@sonar55.ru>
2020-09-22 15:25:05 +0600

    fighting with bugs

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const Chapter = require('./models/chapter');
 const Product = require('./models/productFull');
 const tunnel = require('tunnel');
 const agent = tunnel.httpsOverHttp({
-  proxy: {
-    host: '193.31.103.37',
-    port: 9477,
-    proxyAuth: 'CcxHv8:6ymQqU'
-  }
+    proxy: {
+        host: '193.31.103.37',
+        port: 9477,
+        proxyAuth: 'CcxHv8:6ymQqU'
+    }
 });
 
 
@@ -20,93 +20,97 @@ const BASE_URL = 'https://www.mirgaza.ru';
 /** Сделать запрос, и вернуть результат калалога с id равным link */
 async function getCatalog(link) {
 
-  // результат уже как удобно сформируй
-  const result = [];
-  const chapters = [];
+    // результат уже как удобно сформируй
+    const result = [];
+    const chapters = [];
 
 
-  try {
-    console.log('Сканирую страницу...' + link);
-    const {data} = await axios({
-      method: 'GET',
-      url: BASE_URL + link,
-      httpsAgent: agent,
-      proxy: false
-    });
+    try {
+        console.log('Сканирую страницу...' + link);
+        const {data} = await axios({
+            method: 'GET',
+            url: BASE_URL + link,
+            httpsAgent: agent,
+            proxy: false
+        });
 
-    const $ = cheerio.load(data);
+        const $ = cheerio.load(data);
 
-    if ($('.group_list').length) {
-      $('.group_list').find('.group_list_item').each(function () {
-        chapters.push(new Chapter($(this).attr('title'), link, false, $(this).attr('href')))
-      });
+        if ($('.group_list').length) {
+            $('.group_list').find('.group_list_item').each(function () {
+                chapters.push(new Chapter($(this).attr('title'), link, false, $(this).attr('href')))
+            });
 
-      result.push(...chapters);
+            result.push(...chapters);
 
-      for (let i = 0; i < chapters.length; i++) {
-        result.push(...await getCatalog(chapters[i].link));
-      }
+            for (let i = 0; i < chapters.length; i++) {
+                result.push(...await getCatalog(chapters[i].link));
+            }
 
-      console.log('Страница ' + link + ' просканирована');
+            console.log('Страница ' + link + ' просканирована');
 
-    } else if ($('.shop_block').length) {
-      console.log(link + ' Дошли до товаров в этой категории');
-      $('.shop_table').find('div.description_sell').each(function () {
-        result.push(new Chapter($(this).text().trim(), link, true, $(this).find('a').attr('href')))
-      });
+        } else if ($('.shop_block').length) {
+            console.log(link + ' Дошли до товаров в этой категории');
+            $('.shop_table').find('div.description_sell').each(function () {
+                result.push(new Chapter($(this).text().trim(), link, true, $(this).find('a').attr('href')))
+            });
+        }
+
+        return result;
+
+    } catch (e) {
+        throw new Error(e);
     }
-
-    return result;
-
-  } catch (e) {
-    throw new Error(e);
-  }
 }
 
 async function getProductDataItem(link) {
 
-  const tempArr = [];
-  const productData = [];
+    const tempArr = [];
+    const productData = [];
 
-  try {
-    const {data} = await axios({
-      method: "GET",
-      url: BASE_URL + link,
-      httpsAgent: agent,
-      proxy: false
-    });
+    try {
+        const {data} = await axios({
+            method: "GET",
+            url: BASE_URL + link,
+            httpsAgent: agent,
+            proxy: false
+        });
 
-    const $ = cheerio.load(data);
+        const $ = cheerio.load(data);
 
-    $('div.shop_full_item_right').find('span.full_title').each(function () {
-      if ($(this).text().length) {
-        tempArr.push($(this).text())
-        // $('div.shop_full_item_tabs').find('.box').text()
-      }
-    })
+        $('div.shop_full_item_right').find('span.full_title').each(function () {
+            if ($(this).text().length) {
+                tempArr.push($(this).text())
+            }
+        })
 
-    tempArr.push($('div.shop_full_item_tabs').find('.box').first().text().trim());
+        tempArr.push($('div.shop_full_item_tabs').find('.box').first().text().trim());
 
-    // productData.push(new Product($('img.shop_full_item_img').attr('src'), tempArr[0], tempArr[1], $('span.price_value').text(), tempArr[2]));
-
-    return tempArr;
-  } catch (error) {
-    throw new Error(error)
-  }
+        return new Product($('img.shop_full_item_img').attr('src'), tempArr[0], tempArr[1], $('span.price_value').text(), tempArr[2], tempArr[tempArr.length - 1], $('div.path').find('a')[length - 1].text.trim());
+    } catch (error) {
+        throw new Error(error)
+    }
 }
 
 async function init() {
 
-    // const catalog1 = await getCatalog('/catalog/');
-    //
-    // const arr = catalog1.filter(e => e.isProduct);
-    // console.log(arr);
-    // console.log('Готово');
+    try {
 
-  const arr = await getProductDataItem('/catalog/magistral-i-shlangi/pipe-faro-6-mm-thermoplastic/');
-  // console.log(arr);
-  console.log(arr);
-  console.log(arr.length);
+        const catalog1 = await getCatalog('/catalog/');
+        const arr = catalog1.filter(e => e.isProduct);
+
+        console.log('Список каталогов с товарами получен. Начинаю получение данных по каждому товару');
+
+        const arrayOfProducts = await arr.map(el => {
+            return getProductDataItem(el.link)
+        })
+        for (let i = 0; i < arrayOfProducts; i++) {
+            console.log(arrayOfProducts[i]);
+            break;
+        }
+    } catch (e) {
+        throw new Error()
+    }
 }
 
 
--- models/productFull.js
+++ models/productFull.js
@@ -5,12 +5,14 @@ class ProductFull {
 // Детальное описание
 // цена
 // производитель
-    constructor(imgSrc, title, vendorCode = 0, cost = 0, manufacturerCode = 0) {
+    constructor(imgSrc, title, vendorCode = 0, cost = 0, manufacturerCode = 0, description = '-', parent) {
         this.imgSrc = imgSrc;
         this.title = title;
         this.vendorCode = vendorCode;
         this.cost = cost;
         this.manufacturerCode = manufacturerCode;
+        this.description = description;
+        this.parent = parent;
     }
 }
 
